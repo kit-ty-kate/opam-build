2e31c1ce0f70afc1bd88f284ae2a7d9d56041be1
Kate <kit.ty.kate@disroot.org>
2021-11-22 15:36:31 +0000

    Use OpamAction.build_package to get and run the build commands

--- src/main.ml
+++ src/main.ml
@@ -12,44 +12,40 @@ let get_pkg (name, opam, _) =
   let opam = OpamFile.OPAM.read opam in
   let version = match OpamFile.OPAM.version_opt opam with
     | Some v -> v
-    | None -> OpamPackage.Version.of_string "dev"
+    | None -> OpamPackage.Version.of_string "~dev"
   in
-  let package = OpamPackage.create name version in
-  (package, opam)
+  OpamPackage.create name version
 
-let check_dependencies st =
+let check_dependencies st dirname =
   let st, atoms =
-    OpamAuxCommands.autopin st ~recurse:false ~quiet:true ~simulate:true
-      [`Dirname (OpamFilename.Dir.of_string ".")]
+    OpamAuxCommands.simulate_autopin st ~recurse:false ~quiet:true [`Dirname dirname]
   in
   let missing = OpamClient.check_installed ~build:true ~post:true st atoms in
-  if OpamPackage.Map.is_empty missing then
-    ()
-  else
+  if not (OpamPackage.Map.is_empty missing) then
     (* TODO: do the thing instead of telling the user to do it *)
-    OpamConsole.error_and_exit `Bad_arguments "Dependencies are missing. Hint: opam install --deps-only ."
+    OpamConsole.error_and_exit `Bad_arguments "Dependencies are missing. Hint: opam install --deps-only .";
+  st
+
+let rec iter_job = function
+  | OpamProcess.Job.Op.Done _ -> ()
+  | Run (cmd, k) ->
+      print_endline ("  - "^OpamProcess.string_of_command cmd);
+      let result = OpamProcess.run cmd in
+      iter_job (k result)
 
 let () =
+  (* TODO: Pass it as parameter? *)
+  let dirname = OpamFilename.Dir.of_string "." in
+  (* TODO: Disable sandbox by default? Make it configurable? *)
   OpamClientConfig.opam_init ();
   OpamGlobalState.with_ `Lock_none @@ fun gt ->
   OpamSwitchState.with_ `Lock_write gt @@ fun st ->
   (* TODO: setup a local switch by default, but make it configurable *)
-  check_dependencies st;
-  OpamAuxCommands.opams_of_dir (OpamFilename.Dir.of_string ".") |>
+  let st = check_dependencies st dirname in
+  OpamAuxCommands.opams_of_dir dirname |>
   List.map get_pkg |>
-  List.iter (fun (package, opam) ->
+  List.iter (fun package ->
+    let job = OpamAction.build_package st ~test:false ~doc:false dirname package in
     print_endline ("Building "^OpamPackage.to_string package^"...");
-    OpamFile.OPAM.build opam |>
-    OpamFilter.commands (OpamPackageVar.resolve_switch ~package st) |>
-    List.iter (function
-      | [] -> OpamConsole.error_and_exit `Bad_arguments "Empty command, aborting..."
-      | (cmd::_ as args) ->
-          print_endline ("  - "^String.concat " " args);
-          let args = Array.of_list args in
-          (* TODO: Handle build-env *)
-          let pid = Unix.create_process cmd args Unix.stdin Unix.stdout Unix.stderr in
-          match waitpid pid with
-          | 0 -> ()
-          | n -> OpamConsole.error_and_exit `Bad_arguments "Command exited with code %d" n
-    )
+    iter_job job
   )
